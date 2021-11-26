f9acc89ce6455ac74c908575da578766e787507f
m3m3ng01 <antarsayuridn@gmail.com>
2021-11-26 20:22:18 +0700

    first commit

---
+++ fizzbuzz1/fizzbuzz1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan nilai yang diuji : ");
+            int nilai = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= nilai; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.Write("FizzBuzz" + " ");
+
+                else if (i % 3 == 0)
+                    Console.Write("Fizz" + " ");
+
+                else if (i % 5 == 0)
+                    Console.Write("Buzz" + " ");
+                else
+                    Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
